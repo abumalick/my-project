6fabf20ffdf4c1185db20764c6648a40f9671f9b
abumalick <abdullah.hilson@gmail.com>
2020-07-03 10:04:38 +0100

    feat: add sentry middleware according to documentation
    https://strapi.io/documentation/v3.x/guides/error-catching.html#handle-errors

---
+++ middlewares/sentry/index.js
@@ -0,0 +1,23 @@
+const Sentry = require("@sentry/node");
+console.log("INITIALIZING SENTRY");
+Sentry.init({
+  dsn:
+    "https://77ac1ecb19d4446cafa3f1590221db6b@o415410.ingest.sentry.io/5306847",
+  environment: strapi.config.environment,
+});
+
+module.exports = (strapi) => {
+  return {
+    initialize() {
+      strapi.app.use(async (ctx, next) => {
+        try {
+          await next();
+        } catch (error) {
+          console.log("SENDING EXCEPTION TO SENTRY");
+          Sentry.captureException(error);
+          throw error;
+        }
+      });
+    },
+  };
+};
